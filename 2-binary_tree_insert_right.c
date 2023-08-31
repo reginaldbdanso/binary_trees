fba4f1a8d675679fa15b7e83bef4ffbd4a169275
reginaldbdanso <reginaldbdanso@gmail.com>
2023-08-31 20:21:58 +0000

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,11 +11,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (parent == NULL)
-		return (NULL);
-
 	if (new_node == NULL)
 		return (NULL);
+
+	if (parent == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	new_node->n = value;
 	new_node->parent = parent;
