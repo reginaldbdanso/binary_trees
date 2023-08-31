48a764f14acb61c17ec14768e2128fd45ee962a7
reginaldbdanso <reginaldbdanso@gmail.com>
2023-08-31 21:49:42 +0000

    Updated 5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_is_root - pointer to the node to check
- * @node: pointer to the root node of the tree to delete
+ * @node: pointer to the node to check
  * Return: 1 if node is a root, otherwise 0. If node is NULL, return 0
  */
 
