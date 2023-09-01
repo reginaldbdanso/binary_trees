ffe3deb9c6c901e15b0b30ee8e3ab2d6f8617097
reginaldbdanso <reginaldbdanso@gmail.com>
2023-09-01 00:17:44 +0000

    Updated 13-binary_tree_nodes.c

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * Return: the number of leaves or 0 if tree is NULL. A NULL pointer
+ * is not a leaf
+ */
+
